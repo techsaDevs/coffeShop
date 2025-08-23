b3ba818745d10d10a98cb33f9f0183ae1e381081
amir-Quf <amirquf.com@gmail.com>
2025-08-23 15:43:50 +0330

    completed login page

--- src/validations/validation.ts
+++ src/validations/validation.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup'
 
 export const loginSchema = yup.object().shape({
-    information : yup.string().min(11).required(),
-    password: yup.string().min(5).max(20).required()
+    information : yup.string().min(11, 'حداقل باید 11 کاراکتر وارد کنید').required('وارد کردن این فیلد اجباری است'),
+    password: yup.string().min(5, 'حداقل باید 5 کاراکتر وارد کنید').max(20, 'حداکثر میتوانید 20 کاراکتر وارد کنید').required('وارد کردن این فیلد اجباری است')
 })
 
