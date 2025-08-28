5d0ba7aa6d9272d015deae905f2ab1d1ab0658d1
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-28 12:44:32 +0330

    fix top section footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -9,6 +9,8 @@ import PhoneSVG from './SVGs/home/contact/PhoneSVG'
 import ShapeSVG from './SVGs/home/landing/ShapeSVG'
 import FooterBtn from './Footer/FooterBtn'
 import QuickAccess from './Footer/QuickAccess'
+import TelegramSVG from './SVGs/footer/TelegramSVG'
+import InstagramSVG from './SVGs/footer/InstagramSVG'
 
 const Footer = () => {
 
@@ -64,14 +66,14 @@ const Footer = () => {
         <FooterBtn />
       </div>
 
-      <div className="flexBetween py-7">
+      <div className="flexBetween py-7 border-b border-gray-300/20">
         <div className="">
           <div className="flex items-center gap-4">
             <LogoSVG className='size-12' />
             <LogoTypeSVG className="w-28" />
             <img src="/footer/techsaDevTeam--dark.logo.png" alt="" className='footer-logo w-26' />
           </div>
-          <p className="max-w-[360px] leading-9" style={{ letterSpacing: 20 }}>
+          <p className="max-w-[425px] mt-3 leading-9" style={{ letterSpacing: 20 }}>
             ما برآنیم تا با پیشرو در فرآیند  تولید ، نوع کیفیت محصول، خدمات و
             توزیع، الگویی برای تولید کنندگان ایرانی باشیم و به مرجع فرهنگ در ایران
             تبدیل شویم. می پنداریم که نظر مردم ایران منطقه باید نسبت به کالای ایرانی
@@ -86,28 +88,34 @@ const Footer = () => {
         <div className="">
           <h3 className="footer-title">در تماس باشیم</h3>
           <div className="">
-            <div className="">
+            <div className="flex gap-3 pb-3.5">
               <LocationSVG />
               <p className="">بلوار میرداماد، خیابان البرز، کوچه قبادان شرقی ، پلاک 33</p>
             </div>
-            <div className="">
-              <div className="">
-                <MessageSVG />
+            <div className="flex items-center gap-5">
+              <div className="flex gap-3">
+                <MessageSVG className='-mt-0.5 size-6' />
                 <span className="">techsa.content@gmail.com</span>
               </div>
-              <div className="">
-                <PhoneSVG />
-                <div className="">
-                  <span className="">0902</span>
-                  <span className="">123</span>
+              <div className="flex gap-3">
+                <PhoneSVG className='-mt-1 size-6' />
+                <div className="flex gap-1.5">
                   <span className="">6628</span>
+                  <span className="">123</span>
+                  <span className="">0902</span>
                 </div>
               </div>
-              <span className="">021 - 6789012</span>
+              <span className="">6789012 - 021</span>
             </div>
-            <div className="">
-              <Link href="https://www.instagram.com/techsa.ir" className="">@techsa.ir</Link>
-              <Link href="https://t.me/techsa_ir" className=''>@techsa_ir</Link>
+            <div className="flex items-center gap-6 mt-6">
+              <Link href="https://www.instagram.com/techsa.ir" className="flex items-center gap-3 py-2 px-10 duration-150 rounded-xl border border-orange-300 text-orange-300  hover:bg-orange-300 hover:text-white">
+                <InstagramSVG className="size-8 shrink-0 -mt-[4px]" />
+                <p className="text-lg -mb-[2px]">techsa.ir @</p>
+              </Link>
+              <Link href="https://t.me/techsa_ir" className='flex items-center gap-3 py-2 px-10 duration-150 rounded-xl bg-gradient-to-r text-black hover:text-white from-orange-50 to-orange-300'>
+                <TelegramSVG className="size-8 shrink-0 -mt-[4px]" />
+                <p className="text-lg -mb-[2px]">techsa_ir @</p>
+              </Link>
             </div>
           </div>
         </div>
